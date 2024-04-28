5eb57536dc01ec16fe1aacbb72d8f13985e643fb
Morteza Mohiuodin <mortezamohiuodin@gmail.com>
2024-04-28 18:52:00 +0330

    add

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div><button @click="getUser">getUser</button></div>
-  <button @click="test">test</button>
+  <div class="grid grid-cols-5">
+    <CardProduct
+      v-for="product in products"
+      :item="product"
+      :key="product.id"
+    />
+  </div>
 </template>
 <script setup>
 definePageMeta({
@@ -9,6 +14,7 @@ definePageMeta({
 
 const { $adminApi } = useNuxtApp();
 const data = await $adminApi.getCurrentUser();
+const { products } = await $adminApi.getAdminProducts();
 
 const test = () => {
   console.log(token.value, "token");
--- services/admin/products/index.js
+++ services/admin/products/index.js
@@ -1,6 +1,7 @@
 export const getAdminProducts = async () => {
+  const headers = getRequestHeaders();
   const { $medusa } = useNuxtApp();
-  const res = await $medusa.admin.products.list();
+  const res = await $medusa.admin.products.list({}, headers);
 
   return res;
 };
